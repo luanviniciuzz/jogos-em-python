ecf217ba3ca6aecc219c14c0b648e70163337c23
Luan Vinícius <luanviniciuzz@gmail.com>
2022-02-12 18:52:33 -0300

    arquivo do jogo adivinhacao

---
+++ adivinhacao.py
@@ -0,0 +1,10 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Adivinhação!***")
+    print("*********************************")
+
+  
+
+##roda como função principal 
+if(__name__ == "__main__"):
+    jogar()
